c304c20e872061cea448c88c56e04fef6422c25b
Son of Isaac <isaacmulemfwe@gmail.com>
2019-06-13 23:20:58 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,5 +1,6 @@
 // your solution should go here
 // for demo purposes use the console for any out you will need.
+//recipe object
 var recipe = {
   title: "Breaded chicken",
   servings: 4,
@@ -8,3 +9,41 @@ var recipe = {
 document.write(recipe.title+"<br>");
 document.write("Serves: "+recipe.servings+"<br>");
 document.write("Ingredients: "+recipe.ingredients+"<br>");
+//book object
+var books = [
+  {
+    title: "The Hobbit",
+    author : "J.R.R. Tolkien",
+    alreadyRead : false
+  },
+  {
+    title: "The Lord of the Rings",
+    author : "J.R.R. Tolkien",
+    alreadyRead : true
+  },
+  {
+    title: "Born A Crime",
+    author : "Trevor Noah",
+    alreadyRead : true
+  }
+];
+//first book
+document.write(books[0].title+" ");
+document.write(books[0].author+"<br>");
+//second book
+document.write(books[1].title+" ");
+document.write(books[1].author+"<br>");
+//third book
+document.write(books[2].title+" ");
+document.write(books[2].author+"<br>");
+//if/else statement
+if (alreadyRead = true){
+  document.write("You already read "+books[1].title+" ");
+  document.write(books[1].author+"<br>");
+  document.write("You already read "+books[2].title+" ");
+  document.write(books[2].author+"<br>");
+}
+else{
+  document.write("You already read "+books[0].title+" ");
+  document.write(books[0].author+"<br>");
+}
