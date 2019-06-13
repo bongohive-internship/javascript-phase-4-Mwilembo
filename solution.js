842ced9f9820a056008912605d29d448e767256d
Son of Isaac <isaacmulemfwe@gmail.com>
2019-06-13 22:01:23 +0200

    Update solutions.js

--- solution.js
+++ solution.js
@@ -1,2 +1,10 @@
 // your solution should go here
 // for demo purposes use the console for any out you will need.
+var recipe = {
+  title: "Breaded chicken",
+  servings: 4,
+  ingredients: ["chicken","breadcrumbs","eggs","flour"]
+};
+console.log(recipe.title);
+console.log(recipe.servings);
+console.log(recipe.ingredients);
